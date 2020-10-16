30423b196ecdfde63590def628d61160e9749dfd
brandonredm <70499844+brandonredm@users.noreply.github.com>
2020-10-16 15:25:57 -0400

    I think it's good to go on CRUD
    updated controllers/job.php and models/job.php

--- controllers/jobs.php
+++ controllers/jobs.php
@@ -7,14 +7,28 @@ if($_REQUEST['action'] === 'index'){
   $request_body = file_get_contents('php://input');
   $body_object = json_decode($request_body);
 
-  $new_job = new Job(null, $body_object->name, $body_object->age);
+  $new_job = new Job(null, $body_object->company,
+    $body_object->position,
+    $body_object->application_link,
+    $body_object->resources_link,
+    $body_object->contact,
+    $body_object->notes,
+    $body_object->interest_level,
+    $body_object->interviews);
 //  print_r($new_job);
   $all_jobs = Jobs::create($new_job);
   echo json_encode($all_jobs);
 } else if ($_REQUEST['action'] === 'update') {
   $request_body = file_get_contents('php://input');
   $body_object = json_decode($request_body);
-  $updated_job = new Job($_REQUEST['id'], $body_object->name, $body_object->age);
+  $updated_job = new Job($_REQUEST['id'], $body_object->company,
+    $body_object->position,
+    $body_object->application_link,
+    $body_object->resources_link,
+    $body_object->contact,
+    $body_object->notes,
+    $body_object->interest_level,
+    $body_object->interviews);
   $all_jobs = Jobs::update($updated_job);
   echo json_encode($all_jobs);
 } else if ($_REQUEST['action'] === 'delete') {
--- models/job.php
+++ models/job.php
@@ -85,4 +85,5 @@ class Jobs {
         }
         return $jobs;
     }
+	}
  ?>
