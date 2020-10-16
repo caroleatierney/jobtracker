0cd4abb5ee3df5f7b6fe16d150574e0480ffa4e4
brandonredm <70499844+brandonredm@users.noreply.github.com>
2020-10-16 14:58:55 -0400

    Doesn't work
    How do I get localhost:8888/jobs to show

--- controllers/jobs.php
+++ controllers/jobs.php
@@ -1,3 +1,24 @@
 <?php
+header('Content-Type: application/json');
+include_once __DIR__ . '/../models/job.php';
+if($_REQUEST['action'] === 'index'){
+  echo json_encode( Jobs::all() );
+} elseif ($_REQUEST['action'] === 'create') {
+  $request_body = file_get_contents('php://input');
+  $body_object = json_decode($request_body);
 
+  $new_job = new Job(null, $body_object->name, $body_object->age);
+//  print_r($new_job);
+  $all_jobs = Jobs::create($new_job);
+  echo json_encode($all_jobs);
+} else if ($_REQUEST['action'] === 'update') {
+  $request_body = file_get_contents('php://input');
+  $body_object = json_decode($request_body);
+  $updated_job = new Job($_REQUEST['id'], $body_object->name, $body_object->age);
+  $all_jobs = Jobs::update($updated_job);
+  echo json_encode($all_jobs);
+} else if ($_REQUEST['action'] === 'delete') {
+  $all_jobs = Jobs::delete($_REQUEST['id']);
+  echo json_encode($all_jobs);
+}
  ?>
