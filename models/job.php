ca6757e611c86d1b1a221cd3c9ce2ed72ecfb112
Mimi Netto <michnetto249@gmail.com>
2020-10-19 12:59:01 -0400

    db trouble shoot

--- models/job.php
+++ models/job.php
@@ -4,7 +4,7 @@ $dbconn = null;
 if(getenv('postgres://kmtfwdxzrdfehd:3d9c7078eded57223b087b050e02b68a599004ebee734599805b3ded875c8a8e@ec2-18-211-86-133.compute-1.amazonaws.com:5432/dehs9nr7f8vfhk')){ // if using the heroku database
 	$connectionConfig = parse_url(getenv('postgres://kmtfwdxzrdfehd:3d9c7078eded57223b087b050e02b68a599004ebee734599805b3ded875c8a8e@ec2-18-211-86-133.compute-1.amazonaws.com:5432/dehs9nr7f8vfhk'));
 	$host = $connectionConfig['ec2-18-211-86-133.compute-1.amazonaws.com'];
-	$user = $connectionConfig['dehs9nr7f8vfhk'];
+	$user = $connectionConfig['kmtfwdxzrdfehd'];
 	$password = $connectionConfig['3d9c7078eded57223b087b050e02b68a599004ebee734599805b3ded875c8a8e'];
 	$port = $connectionConfig['5432'];
 	$dbname = trim($connectionConfig['dehs9nr7f8vfhk'],'/');
